40e9bd11e5db7281fd44f9529e85225fb41b757a
Praneeth Maddirala <PMaddira@its.jnj.com>
2023-07-31 23:18:15 +0530

    Auto-generated commit [ci skip]

--- src/app/api/products-api.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from "@angular/core";
-import { ProductsClientBase } from "@generated-api-clients";
-@Injectable({
-    providedIn: "root"
-})
-export class ProductsApiService extends ProductsClientBase {}
--- src/app/api/ui-localization-api.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@angular/core";
-import { UiLocalizationClientBase } from "@generated-api-clients";
-
-@Injectable({
-    providedIn: "root"
-})
-export class UiLocalizationApiService extends UiLocalizationClientBase {}
